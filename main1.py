4f8adf79c9927654a4d1aed70d7c7f842e84b99a
Jung-Haekwang <hkjung87@gmail.com>
2024-05-24 11:40:18 +0900

    Update main1.py

--- main1.py
+++ main1.py
@@ -9,7 +9,7 @@ from langchain.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 import streamlit as st
 import tempfile
